1faf576baebd182b909c58c0e077041b02eb09ba
biubiujch <coolechi@foxmail.com>
2022-08-29 10:48:40 +0800

    feat:axios 可用

--- src/pages/projectManage/index.tsx
+++ src/pages/projectManage/index.tsx
@@ -1,5 +1,21 @@
+import { Button, Modal } from "antd";
+import { useState } from "react";
+import { Wrap } from "./index.styled";
+
 function ProjectManage() {
-  return <div>项目管理</div>;
+  const [visible, setVisible] = useState<boolean>(false);
+
+  return (
+    <Wrap>
+      <Button type="primary" onClick={() => setVisible(true)}>
+        创建项目
+      </Button>
+      <div></div>
+      <Modal visible={visible} onCancel={() => setVisible(false)} onOk={() => setVisible(false)}>
+        <div>创建项目表单</div>
+      </Modal>
+    </Wrap>
+  );
 }
 
 export default ProjectManage;
