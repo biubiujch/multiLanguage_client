ccbc7e65b396a121f8133abf6e8b181b593bfd0e
biubiujch <coolechi@foxmail.com>
2022-08-29 16:05:06 +0800

    udpate: 新增 login 状态

--- src/pages/projectManage/index.tsx
+++ src/pages/projectManage/index.tsx
@@ -1,16 +1,25 @@
-import { Button, Modal } from "antd";
+import { Button, Modal, Table } from "antd";
+import { ColumnsType } from "antd/lib/table";
 import { useState } from "react";
-import { Wrap } from "./index.styled";
+import { Wrap, TableWrap } from "./index.styled";
 
 function ProjectManage() {
   const [visible, setVisible] = useState<boolean>(false);
 
+  const columns: ColumnsType<any> = [
+    { title: "项目名称", dataIndex: "projectName", key: "id" },
+    { title: "创建人", dataIndex: "administrator", key: "administratorID" },
+    { title: "操作", render: () => <span>删除 详情</span> }
+  ];
+
   return (
     <Wrap>
-      <Button type="primary" onClick={() => setVisible(true)}>
+      <Button type='primary' onClick={() => setVisible(true)}>
         创建项目
       </Button>
-      <div></div>
+      <TableWrap>
+        <Table columns={columns} dataSource={[]} />
+      </TableWrap>
       <Modal visible={visible} onCancel={() => setVisible(false)} onOk={() => setVisible(false)}>
         <div>创建项目表单</div>
       </Modal>
